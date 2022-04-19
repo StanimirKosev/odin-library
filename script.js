5e19e9bb274d2186e3ff180ad19d608bf8e6c6c9
Stanimir <stanimirkosev00@gmail.com>
2022-04-19 10:08:03 +0300

    display value

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 let myLibrary = [];
 
-function Book(title,author,pages,readStatus){ /**object constructor */
+function Book(title,author,pages,readStatus){
   this.title = title
   this.author = author
   this.pages = pages
   this.readStatus = readStatus
 }
 
-function addBookToLibrary(){ /** add event listener's */
-  let title = window.prompt("What's the book title?");
+function addBookToLibrary(){ 
+  let title = window.prompt("What's the book title?");  
   let author = window.prompt("Who is the author?");
   let pages = window.prompt("How much pages is the book?");
   let readStatus = window.prompt("Is the book read or unread?");
   
   const book = new Book(title,author,pages,readStatus); 
-  
+
   myLibrary.push(book);
-  return book.info();
+  return book.info(); 
 }
 
 Book.prototype.info = function(){
-  return `${this.title} of ${this.author}, ${this.pages} pages, ${this.readStatus}` 
+  const text = document.querySelector('.text');
+  let bookCard = document.createElement('div');
+  text.appendChild(bookCard);
+  bookCard.textContent = `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}` 
 }
 
-console.log(addBookToLibrary());
-console.log(addBookToLibrary());
-console.log(myLibrary); /**add 2 buttons */
+const addBook = document.querySelector('.addBook');
+addBook.addEventListener('click',()=>{ 
+  addBookToLibrary()
+  });
+
+
 
 
