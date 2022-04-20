66c36f2569d19dc7833d05d29bdf530149cd4662
Stanimir <stanimirkosev00@gmail.com>
2022-04-20 12:13:43 +0300

    modal window

--- script.js
+++ script.js
@@ -7,22 +7,6 @@ function Book(title,author,pages,readStatus){
   this.readStatus = readStatus
 }
 
-function defaultBook(){
-  const book = new Book('The Almanack of Naval Ravikant:A Guide to Wealth and Happiness','Eric Jorgenson','244','read');
-  myLibrary.push(book);
-  return book.info(); 
-}
-
-function addBookToLibrary(){ 
-  let title = window.prompt("What's the book title?");  
-  let author = window.prompt("Who is the author?");
-  let pages = window.prompt("How much pages is the book?");
-  let readStatus = window.prompt("Is the book read or unread?");
-  const book = new Book(title,author,pages,readStatus); 
-  myLibrary.push(book);
-  return book.info(); 
-}
-
 Book.prototype.info = function(){
   const parent = document.querySelector('.parent');
   let bookCard = document.createElement('button');
@@ -30,15 +14,70 @@ Book.prototype.info = function(){
   bookCard.textContent = `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus}` 
 }
 
-const addBook = document.querySelector('.addBook');
-addBook.addEventListener('click',()=>{ 
-  addBookToLibrary()
-  });
-
+function defaultBook(){
+  const book = new Book('The Almanack of Naval Ravikant:A Guide to Wealth and Happiness','Eric Jorgenson','244','read');
+  myLibrary.push(book);
+  return book.info(); 
+}
 defaultBook();
-/**Prevents pop up window when refreshing, caused by the default book displayed*/
+
+function addBookToLibrary(title,author,pages,readStatus){ 
+  const book = new Book(title,author,pages,readStatus); 
+  myLibrary.push(book);
+  return book.info(); 
+}
+
+document.querySelector('.submitButton').onclick = function(){
+  let title = document.getElementById('title').value;
+  let author = document.getElementById('author').value;
+  let pages = document.getElementById('pages').value;
+  let readStatus = document.getElementById('readStatus').value;
+  addBookToLibrary(title,author,pages,readStatus);
+  closeModal(modal);
+}
+
+/********** Modal ***********/
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget) /**data attributes - selects the #modal and passes it to the function*/
+    openModal(modal);
+  })
+});
+
+function openModal(modal){
+  if (modal == null ) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal') /** selects the closest parent */
+    closeModal(modal);
+  })
+});
+
+function closeModal(modal){
+  if (modal == null ) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+overlay.addEventListener('click',() => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal);
+  })
+})
+
+
+
+/**Prevents chrome pop up window when refreshing, caused by the default book displayed*/
 if ( window.history.replaceState ) {
   window.history.replaceState( null, null, window.location.href );
 }
-
-
